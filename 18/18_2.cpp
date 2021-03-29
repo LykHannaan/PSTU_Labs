a7391a1a36ac0e615c1537f17e98147b23b3b32d
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2021-03-29 16:34:03 +0500

    Update 18_2.cpp

--- 18/18_2.cpp
+++ 18/18_2.cpp
@@ -1,1 +1,37 @@
-
+#include<iostream>
+#include<string>
+#include "zarplata.h"
+using namespace std;
+ZP make_zarplata()
+{
+	string s;
+	double i;
+	int d;
+	cout << "Enter FIO: ";
+	cin.get();
+	getline (cin, s);
+	cout << "Enter salary: ";
+	cin >> i;
+	cout << "Enter percent: ";
+	cin >> d;
+	ZP t(s, i, d);
+	return t;
+}
+void print_zarplata(ZP t)
+{
+	t.show();
+}
+void main()
+{
+	ZP t1;
+	t1.show();
+	ZP t2("Karelov D.S.", 120.5, 30);
+	t2.show();
+	ZP t3 = t2;
+	t3.set_FIO("Avronin M.A.");
+	t3.set_salary(240.7);
+	t3.set_percent(21);
+	print_zarplata(t3);
+	t1 = make_zarplata();
+	t1.show();
+}
