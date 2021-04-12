d0e3d3b2c6db7d649be536e7ba7794c4bcae1930
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2021-04-12 14:45:26 +0500

    Update 18_3.cpp

--- 18/3/18_3.cpp
+++ 18/3/18_3.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "Pair.h"
+using namespace std;
+const int z = 22;
 
+void main()
+{
+	Pair a;
+	Pair b;
+	Pair c;
+	cin >> a;
+	cin >> b;
+	c = a - b;
+	cout << "c=" << c << endl;
+	cout << "a=" << a + z << endl;
+	cout << "b=" << b + z << endl;
+}
