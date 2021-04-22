2a25859c0567802bdfc52ed0698d6e7ae7bb5301
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2021-04-22 14:10:10 +0500

    Update 18_1.cpp

--- 18/1/18_1.cpp
+++ 18/1/18_1.cpp
@@ -1,63 +1,75 @@
-﻿#include <iostream>
+#include <iostream>
 #include "fraction.h"
 #include "fraction.cpp"
 
 using namespace std;
 
-fraction make_fraction(double F, int S, int G)
+progression make_progression(double F, int S)
 {
-	fraction t;
-	t.Init(F, S, G);
+	progression t;
+	t.Init(F, S);
 	return t;
 }
+
 int main()
 {
-	fraction A;
-	fraction B;
-	A.Init(3.0, 2, 4);
-	B.Read();
+	progression A;
+	progression B;
+	int j;
+	A.Init(3.0, 2);
+	cout << "Введите номер элемента прогрессии: ";
+	cin >> j;
+	cout << "A.element(" << A.first << "," << A.second << ")=" << A.element(j) << endl;
 	A.Show();
+
+	B.Read();
+	cout << "Введите номер элемента прогрессии: ";
+	cin >> j;
+	cout << "B.element(" << B.first << "," << B.second << ")=" << B.element(j) << endl;
 	B.Show();
-	cout << "A.element(" << A.first << "," << A.second << ")=" << A.element() << endl;
-	cout << "B.element(" << B.first << "," << B.second << ")=" << B.element() << endl;
-	fraction* X = new fraction;
-	X->Init(2.0, 5, 6);
+
+	progression* X = new progression;
+	X->Init(2.0, 5);
 	X->Show();
-	X->element();
-	cout << "X.Power(" << X->first << "," << X->second << ")=" << X->element() << endl;
-	fraction mas[3];
+	X->element(j);
+	cout << "Введите номер элемента прогрессии: ";
+	cin >> j;
+	cout << "X.element(" << X->first << "," << X->second << ")=" << X->element(j) << endl;
+	X->Show();
+
+	progression mas[3];
 	for (int i = 0; i < 3; i++)
 		mas[i].Read();
 	for (int i = 0; i < 3; i++)
 		mas[i].Show();
 	for (int i = 0; i < 3; i++)
 	{
-		mas[i].element();
+		mas[i].element(i);
 		cout << "mas[(" << i << "].element(" << mas[i].first << "," << mas[i].second << ")=";
-		cout << mas[i].element() << endl;
+		cout << mas[i].element(i) << endl;
 	}
 
-	fraction* p_mas = new fraction[3];
+	progression* p_mas = new progression[3];
 	for (int i = 0; i < 3; i++)
 		p_mas[i].Read();
 	for (int i = 0; i < 3; i++)
 		p_mas[i].Show();
 	for (int i = 0; i < 3; i++)
 	{
-		p_mas[i].element();
+		p_mas[i].element(i);
 		cout << "p_mas[(" << i << "].element(" << p_mas[i].first << "," << p_mas[i].second << ")=";
-		cout << p_mas[i].element() << endl;
+		cout << p_mas[i].element(i) << endl;
 	}
 
-	double y; int z; int q;
+	double y; int z;
 	cout << "first?";
 	cin >> y;
 	cout << "second?";
 	cin >> z;
-	cout << "j?";
-	cin >> q;
 
-	fraction F = make_fraction(y, z, q);
+	progression F = make_progression(y, z);
 	F.Show();
+	cout << "F.element(" << F.first << "," << F.second << ")=" << F.element(j) << endl;
+
 	return 0;
 }
