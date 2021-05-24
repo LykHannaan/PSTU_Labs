01cb6a771128b6329a427d6b1c7713053f35135c
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2021-05-24 15:59:26 +0500

    Add files via upload

---
+++ 18/9/2/18_9_2.cpp
@@ -0,0 +1,32 @@
+﻿#include "List.h" 
+#include "error.h"
+#include <iostream> 
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	try
+	{
+		List a(5);
+		int bsize;
+		cin >> bsize;
+		List b(bsize); //при вводе bsize > 20 выдаст ошибку 
+		cin >> a;
+		cout << a;
+		cout << "Номер элемента списка: ";
+		int n;
+		cin >> n;
+		cout << a[n] << endl;
+		b = a;
+		b * a;
+		cout << a;
+
+		cout << "Пример попытки обращения к элементу с номером больше текущего размера списка: " << endl;
+		a[7] = 9;
+	}
+	catch (error e)
+	{
+		e.what();
+	}
+	return 0;
+}
